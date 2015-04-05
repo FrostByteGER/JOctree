f4df2bb710ce153111f2b1a5eb3b14d706d9e083
Kevin Kuegler <kekuegler@stud.hs-bremen.de>
2015-04-05 19:22:17 +0200

    Initial Commit

---
+++ JOctree/src/de/frostbyteger/start/JOctree.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package de.frostbyteger.start;
+
+import java.awt.EventQueue;
+
+/**
+ * @author Kevin Kuegler
+ * @version 1.00
+ */
+public class JOctree {
+
+	/**
+	 * 
+	 */
+	public JOctree() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					//PasswordGenGUI frame = new PasswordGenGUI();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
